e46f63b89ac86ac378be6cb868b8a5ec831f8b5f
rolypolyvg295 <22863750+rolypolyvg295@users.noreply.github.com>
2024-02-26 20:34:50 +0900

    Init lab6-1 with scripts

---
+++ pb_lab6/lab6-1/run_scripts/debug_compile.sh
@@ -0,0 +1,18 @@
+PYTHON_SCRIPT_PATH=/build/CA_Summer_Project/types_helper
+TYPE_DEF_FILES=lib/common-lib/ProcTypes.bsv,src/Proc.bsv
+
+
+cd /build/CA_Summer_Project/pb_lab6/lab6-1 
+
+cp src/Proc.bsv src/gdb/Proc.bsv.copy
+python3.9 $PYTHON_SCRIPT_PATH/main.py \
+    --filenames lib/common-lib/Types.bsv,lib/common-lib/ProcTypes.bsv,src/Proc.bsv \
+    --debug_vars src/gdb/debug_vars.xml \
+    --proc src/Proc.bsv \
+    --reg_order src/gdb/regs_bits.txt \
+    --base_xml src/gdb/base.xml \
+    --output_xml src/gdb/custom.xml \
+
+cd src
+
+./risc-v -c -g
---
+++ pb_lab6/lab6-1/run_scripts/normal_compile.sh
@@ -0,0 +1,2 @@
+cd /build/CA_Summer_Project/pb_lab6/lab6-1/src
+./risc-v -c
